a9a4432be9ea1ac11a8b0f1c3cbb46c4dffcacbe
seanlatias <seanlatias@gmail.com>
2018-07-23 20:06:08 +0800

    [Fix] wrong Makefile

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
-source /opt/Xilinx/SDx/2018.1/settings64.sh
+source /opt/Xilinx/SDx/2018.2/settings64.sh
 export XILINXD_LICENSE_FILE=5100@wsr1.ee.nthu.edu.tw:3100@wsr1.ee.nthu.edu.tw:2100@wsr1.ee.nthu.edu.tw
